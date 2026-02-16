97ddd776283ee8f86b919742c95240a1f4eb02e4
Robert Altman <robert@AltmanSoftwareDesign.com>
2026-02-16 15:04:12 -0600

    Fix standard color and print definitions

--- scripts/get-main-package.sh
+++ scripts/get-main-package.sh
@@ -1,17 +1,23 @@
 #!/bin/zsh
 # This script is authored by Robert Altman, OptumRx
 # robert.altman@optum.com
-# Version 1.0.1
+# Version 1.0.2
 
 # Define color codes for terminal output
 COLOR_GREEN="\e[32m"         # Used for success messages and instructions
 COLOR_RED="\e[31m"           # Used for error messages and warnings
 COLOR_YELLOW="\e[33m"        # Used for help text, lists, and informational content
 COLOR_MAGENTA="\e[35m"       # Available for general use
+COLOR_CYAN="\e[36m"          # Available for general use
+COLOR_BLUE="\e[34m"          # Available for general use; does not show on screen well
+COLOR_BRIGHTYELLOW="\e[93m"  # Used for highlighting important actions and status
 COLOR_RESET="\e[0m"          # Used to reset color formatting
 
+# Function to print colored output
 print_colored() {
-    printf "${1}${2}${COLOR_RESET}\n"
+    local color=$1
+    local message=$2
+    printf "${color}${message}${COLOR_RESET}\n"
 }
 
 # Parse arguments
